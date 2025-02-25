ebe508a50b6b43f14b78eaaa58347aa6ba94b991
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-02-25 18:19:09 +0530

    completed get-All-Course & get-Course-By-Id controller routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoutes.js';
 
 
 const app = express();
@@ -32,6 +33,8 @@ app.get('/', (req, res) => {
 app.post('/clerk',express.json(),clerkWebhooks);
 
 app.use('/api/educator',express.json(),educatorRouter);
+
+app.use('/api/course',express.json(),courseRouter);
 
 // Start server
 app.listen(PORT, () => {
