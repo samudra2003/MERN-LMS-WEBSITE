b78684ea1cd0c8ecec19a1fd68d070ceef59a106
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-02-25 17:06:21 +0530

    completed get-Course,educator-dashboard,enrolled-students controllers and routers

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,6 +1,6 @@
 
 import express  from 'express';
-import { addNewCourse, updateRoleToEducator } from '../controllers/educatorController.js';
+import { addNewCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorController.js';
 import upload from '../configs/multer.js';
 import { protectEducator } from '../middlewares/authMiddleware.js';
 
@@ -10,4 +10,8 @@ const educatorRouter = express.Router();
 //Add educator role
 educatorRouter.get('/update-role', updateRoleToEducator);
 educatorRouter.post('/add-course', upload.single('image'),protectEducator,addNewCourse);
+educatorRouter.get('/courses',protectEducator, getEducatorCourses);
+educatorRouter.get('/dashboard',protectEducator,educatorDashboardData);
+educatorRouter.get('/enrolled-students',protectEducator,getEnrolledStudentsData);
+
 export default educatorRouter;
